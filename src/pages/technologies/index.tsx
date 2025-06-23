c6c6d8247faf8b2f4c80170ffe23ac60caaaa01f
Danilo Krausz <danilo.krausz@gmail.com>
2025-06-23 19:10:01 +0200

    Fixed some deploy bugs

--- src/pages/technologies/index.tsx
+++ src/pages/technologies/index.tsx
@@ -1,36 +1,68 @@
 import { Tech } from "../../components/tech";
 import styles from "./technologies.module.css";
 
+// const technologies = {
+//   languages: [
+//     { icon: "../../../public/icons/html5.svg", name: "HTML5" },
+//     { icon: "../../../public/icons/css3.svg", name: "CSS3" },
+//     { icon: "../../../public/icons/js.svg", name: "JavaScript" },
+//     { icon: "../../../public/icons/ts.svg", name: "TypeScript" },
+//     { icon: "../../../public/icons/sql.svg", name: "SQL" },
+//   ],
+
+//   libraries: [
+//     { icon: "../../../public/icons/react.svg", name: "React.js" },
+//     { icon: "../../../public/icons/express.svg", name: "Express.js" },
+//     { icon: "../../../public/icons/prisma.svg", name: "Prisma ORM" },
+//     { icon: "../../../public/icons/jwt.svg", name: "JWT" },
+//     { icon: "../../../public/icons/zod.svg", name: "Zod" },
+//     { icon: "../../../public/icons/axios.svg", name: "Axios" },
+
+//     { icon: "../../../public/icons/tailwind.svg", name: "Tailwind CSS" },
+//   ],
+
+//   tools: [
+//     { icon: "../../../public/icons/git.svg", name: "Git" },
+//     { icon: "../../../public/icons/github.svg", name: "GitHub" },
+//     { icon: "../../../public/icons/npm.svg", name: "NPM Scripts" },
+//     { icon: "../../../public/icons/vite.svg", name: "Vite" },
+//     { icon: "../../../public/icons/eslint.svg", name: "ESLint" },
+//     { icon: "../../../public/icons/prettier.svg", name: "Prettier" },
+//     { icon: "../../../public/icons/faker.svg", name: "Faker.js" },
+//     { icon: "../../../public/icons/insomnia.svg", name: "Insomnia" },
+//     { icon: "../../../public/icons/ts-node.svg", name: "ts-node-dev" },
+//   ],
+// };
 const technologies = {
   languages: [
-    { icon: "../../../public/icons/html5.svg", name: "HTML5" },
-    { icon: "../../../public/icons/css3.svg", name: "CSS3" },
-    { icon: "../../../public/icons/js.svg", name: "JavaScript" },
-    { icon: "../../../public/icons/ts.svg", name: "TypeScript" },
-    { icon: "../../../public/icons/sql.svg", name: "SQL" },
+    { icon: "/icons/html5.svg", name: "HTML5" },
+    { icon: "/icons/css3.svg", name: "CSS3" },
+    { icon: "/icons/js.svg", name: "JavaScript" },
+    { icon: "/icons/ts.svg", name: "TypeScript" },
+    { icon: "/icons/sql.svg", name: "SQL" },
   ],
 
   libraries: [
-    { icon: "../../../public/icons/react.svg", name: "React.js" },
-    { icon: "../../../public/icons/express.svg", name: "Express.js" },
-    { icon: "../../../public/icons/prisma.svg", name: "Prisma ORM" },
-    { icon: "../../../public/icons/jwt.svg", name: "JWT" },
-    { icon: "../../../public/icons/zod.svg", name: "Zod" },
-    { icon: "../../../public/icons/axios.svg", name: "Axios" },
+    { icon: "/icons/react.svg", name: "React.js" },
+    { icon: "/icons/express.svg", name: "Express.js" },
+    { icon: "/icons/prisma.svg", name: "Prisma ORM" },
+    { icon: "/icons/jwt.svg", name: "JWT" },
+    { icon: "/icons/zod.svg", name: "Zod" },
+    { icon: "/icons/axios.svg", name: "Axios" },
 
-    { icon: "../../../public/icons/tailwind.svg", name: "Tailwind CSS" },
+    { icon: "/icons/tailwind.svg", name: "Tailwind CSS" },
   ],
 
   tools: [
-    { icon: "../../../public/icons/git.svg", name: "Git" },
-    { icon: "../../../public/icons/github.svg", name: "GitHub" },
-    { icon: "../../../public/icons/npm.svg", name: "NPM Scripts" },
-    { icon: "../../../public/icons/vite.svg", name: "Vite" },
-    { icon: "../../../public/icons/eslint.svg", name: "ESLint" },
-    { icon: "../../../public/icons/prettier.svg", name: "Prettier" },
-    { icon: "../../../public/icons/faker.svg", name: "Faker.js" },
-    { icon: "../../../public/icons/insomnia.svg", name: "Insomnia" },
-    { icon: "../../../public/icons/ts-node.svg", name: "ts-node-dev" },
+    { icon: "/icons/git.svg", name: "Git" },
+    { icon: "/icons/github.svg", name: "GitHub" },
+    { icon: "/icons/npm.svg", name: "NPM Scripts" },
+    { icon: "/icons/vite.svg", name: "Vite" },
+    { icon: "/icons/eslint.svg", name: "ESLint" },
+    { icon: "/icons/prettier.svg", name: "Prettier" },
+    { icon: "/icons/faker.svg", name: "Faker.js" },
+    { icon: "/icons/insomnia.svg", name: "Insomnia" },
+    { icon: "/icons/ts-node.svg", name: "ts-node-dev" },
   ],
 };
 
